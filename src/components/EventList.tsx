50906406ec762ee150ee69d75e03ebea052987d6
Diana kagotho <dianakago2@gmail.com>
2021-07-29 21:24:43 -0400

    Rendered data from eventlist collection&controller

--- src/components/EventList.tsx
+++ src/components/EventList.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./EventList.module.css";
 import { Link } from "react-router-dom";
+import { useResource } from "react-ketting";
 
 interface IEventProps {
   eventTitle: string;
@@ -9,6 +10,14 @@ interface IEventProps {
   eventEndDate: number;
   eventRsvp?: boolean;
 }
+type EventList = {
+  name: string;
+  type: string;
+  address: number;
+  start_date: number;
+  end_date?: boolean;
+  description: string;
+};
 
 const EventList: React.FC<IEventProps> = ({
   eventTitle,
@@ -17,15 +26,22 @@ const EventList: React.FC<IEventProps> = ({
   eventEndDate,
   eventRsvp,
 }) => {
+  const { loading, error, data } = useResource<EventList>("/Eventlist");
+  if (loading) return <p>Loading...</p>;
+  if (error) return <div className="error">{error.message}</div>;
+
   return (
     <div>
       <div className={styles["event_container"]}>
-        <h2>{eventTitle}</h2>
+        <h2>{data.name}</h2>
+        <h4>{data.type}</h4>
         <div className={styles["details_container"]}>
-          <h3>{eventStartDate}</h3>
-          <h3>{eventEndDate}</h3>
+          <h4>
+            {data.start_date}
+            {data.end_date}
+          </h4>
         </div>
-        <div className={styles["block"]}>{eventDescription}</div>
+        <div className={styles["block"]}>{data.description}</div>
         <div className="btn">
           <Link to="/eventPage">
             <button className={styles["rsvp"]}>RSVP{eventRsvp}</button>
