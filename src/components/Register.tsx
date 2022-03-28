cb787434174cbecf0fe09831bb3d5355a32a7921
Diana Kagotho <dianakago2@gmail.com>
2022-03-28 15:08:29 -0400

    Modified Register component

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -2,16 +2,33 @@ import React from "react";
 import styles from "./Register.module.css";
 import GitHubLogin from "react-github-login";
 import GitHubIcon from "@material-ui/icons/GitHub";
+import { useResource } from "react-ketting";
+import { Resource } from "ketting";
+import { useParams } from "react-router-dom";
 
 interface RegisterProps {
   rsvpGit: string;
 }
 
-const Register: React.FC<RegisterProps> = ({}) => {
+type Register = {
+  id: string;
+}
 
+// function Register({ resource }: { resource: Resource<Register> }) {
+//   const { loading, error, data } = useResource(resource);
+//   if (loading) return <p>Loading...</p>;
+//   if (error) return <div className="error">{error.message}</div>;
+//   const onSuccessGithub = (response:any) => {
+
+const Register: React.FC<RegisterProps> = ({}) => {
+  const {id}: { id: any} = useParams(); 
   const onSuccessGithub = (response:any) => {
     console.log(response.code);
   } 
+ 
+  const { loading, error, data } = useResource<Register>(`/eventlink/${id}`);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <div className="error">{error.message}</div>;
 
   return (
     <div className={styles["form_container"]}>
@@ -24,7 +41,7 @@ const Register: React.FC<RegisterProps> = ({}) => {
             buttonText="RSVP with GitHub"
             className={styles["submit"]}
             valid={true}
-            redirectUri="http://localhost:9000/eventLink">
+            redirectUri= "http://localhost:9000/eventLink/">
             <GitHubIcon style={{ fontSize: "20px", marginRight: "5px" }} /> 
             RSVP with GitHub
             </GitHubLogin>
@@ -33,5 +50,6 @@ const Register: React.FC<RegisterProps> = ({}) => {
     </div>
   );
 };
+
 
 export default Register;
