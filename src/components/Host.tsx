eb156f04c7dcf3744bdaafe8a2968cc6a4ca2481
Diana Kagotho <dianakago2@gmail.com>
2022-03-30 14:48:54 -0400

    Rendered host picture to host component

--- src/components/Host.tsx
+++ src/components/Host.tsx
@@ -12,22 +12,20 @@ type Host = {
   host_name: string;
   host_picture: string;
 };
-
+ 
 
 const Host: React.FC<HostProps> = ({eventHost}) => {
   const {id}: any = eventHost; 
+  const url: string = "";
   const { loading, error, data } = useResource<Host>(`/host/${eventHost}`);
   if (loading) return <p>Loading...</p>;
   if (error) return <div className="error">{error.message}</div>;
-
+  
   return (
     <div className={styles["eventHost_container"]}>
       <h2>Host</h2>
       <h3>{data.host_name}</h3>
-      {data.host_picture && <img className={styles["pic"]} src={data.host_picture[0]} />}
-        {/* {data.host_picture} */}
-        {/* <img src={spongebob} alt="" /> */}
-      {/* </img> */}
+        <img className={styles["pic"]} src={data.host_picture}  alt="host picture"/>  
       {console.log(data.host_picture)}
     </div>
   );
