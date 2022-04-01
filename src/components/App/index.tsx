c56d2d55369d1c3898995c43c602fbe90ff14670
Diana Kagotho <dianakago2@gmail.com>
2022-03-31 21:03:21 -0400

    Removed EventLink from the routes

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import EventList from "../EventList";
 import EventPage from "../EventPage";
 import Edit from "../Edit";
-import EventLink from "../EventLink";
 import { KettingProvider } from "react-ketting";
 import { Client } from "ketting";
 
@@ -67,24 +66,6 @@ const App: React.FC = () => {
                   }
                   eventEndDate={new Date().getTime()}
                   eventShow={undefined}
-                />
-              </Route>
-              <Route exact path="/eventLink">
-                <EventLink
-                  eventTitle="Event2"
-                  attendees=""
-                  eventStartDate={new Date().getTime()}
-                  eventDescription={
-                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
-                  }
-                  eventEndDate={new Date().getTime()}
-                  eventRsvp={undefined}
-                  shareEvent=""
-                  eventSchedule="Event start date: 6:30
-              Workshop code-along: 7:00
-              Q&A or extended chit chat:8:30
-              Event ends:9:00"
-                  eventHost="Host name: Elon Musk, Host image"
                 />
               </Route>
               <Route path="/edit">
